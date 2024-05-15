490458108d569296457206e8ab9685996ff5bd43
serjio <komegunovsergej@gmail.com>
2024-05-15 11:34:12 +0300

    init

---
+++ src/script.js
@@ -0,0 +1,28 @@
+function calculateDistance() {
+  const diameterInput = document.getElementById("diameter");
+  const distanceToTopInput = document.getElementById("distanceToTop");
+  const indexInput = document.getElementById("index");
+  const resultElement = document.getElementById("result");
+
+  const diameter = parseFloat(diameterInput.value);
+  const distanceToTop = parseFloat(distanceToTopInput.value);
+  const index = parseFloat(indexInput.value);
+
+  if (isNaN(diameter) || diameter <= 0 || isNaN(distanceToTop) || distanceToTop < 0 || isNaN(index) || index < 0) {
+    resultElement.textContent = "Пожалуйста, введите корректные данные.";
+    return;
+  }
+  
+  console.log('input params:', {diameter, distanceToTop,index});
+  const distanceToCenter = calculateDistanceToCenter(diameter, distanceToTop, index);
+  resultElement.textContent = "Расстояние от центра диаметра до вершины паза: " + distanceToCenter.toFixed(2);
+}
+
+function calculateDistanceToCenter(diameter, distanceToTop, index) {
+  
+  const slot = distanceToTop - diameter;
+  const radius = diameter / 2;
+  const coefficient = index / 2;  
+
+  return slot + radius + coefficient;
+}
