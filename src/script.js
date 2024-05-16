79f58bc091bdc1a7670e22a9b87eced65ac94f63
serjio <komegunovsergej@gmail.com>
2024-05-16 14:25:22 +0300

    new

--- src/script.js
+++ src/script.js
@@ -1,3 +1,19 @@
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
 function calculateDistance() {
   const diameterInput = document.getElementById("diameter");
   const distanceToTopInput = document.getElementById("distanceToTop");
@@ -14,7 +30,24 @@ function calculateDistance() {
   }
   
   const distanceToCenter = calculateDistanceToCenter(diameter, distanceToTop, index);
-  resultElement.textContent = "Расстояние от центра диаметра до вершины паза: " + distanceToCenter.toFixed(2);
+  resultElement.textContent = "Расстояние от центра диаметра до вершины паза: " + distanceToCenter.toFixed(3);
+}
+
+function calculateGapWidth() {
+  const gapWidthInput = document.getElementById("gapWidth");
+  const indexInputtwo = document.getElementById("indextwo");
+  const resultElement = document.getElementById("gapResult");
+
+  const gapWidth = parseFloat(gapWidthInput.value);
+  const indextwo = parseFloat(indexInputtwo.value);
+
+  if (isNaN(gapWidth) || gapWidth < 0 || isNaN(indextwo) || indextwo < 0) {
+      resultElement.textContent = "Пожалуйста, введите корректные данные.";
+      return;
+  }
+
+  const gapWidthCalculated = calculateGapWidthValue(gapWidth, indextwo);
+  resultElement.textContent = "Номинальная ширина паза " + gapWidth + " и допуск " + indextwo + " середина паза равна " + gapWidthCalculated.toFixed(4);
 }
 
 function calculateDistanceToCenter(diameter, distanceToTop, index) {
@@ -24,4 +57,11 @@ function calculateDistanceToCenter(diameter, distanceToTop, index) {
   const coefficient = index / 2;  
 
   return slot + radius + coefficient;
+}
+
+function calculateGapWidthValue(indextwo, gapWidth) {
+    const mediumWidth = gapWidth / 4;
+    const coefficient1 = indextwo / 2;  
+
+  return coefficient1 + mediumWidth;
 }
